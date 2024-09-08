40eb02d0878f5f52b32d068d10dd06df76a746a7
Akidoo <plunkels@gmail.com>
2024-09-09 00:32:56 +0800

    update delete, move up, and move down

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -12,17 +12,34 @@ function ToDoList(){
 
     function addTask(){
 
+        if(newTask.trim() !== ""){
+        setTasks(t => [...t, newTask]);
+        setNewTask("");
+        }
     }
 
     function deleteTask(index){
 
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index -1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+
 
     }
 
     function moveTaskDown(index){
+        if(index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
 
     }
 
